68773436503950981f1152ff0bfa98edd815d1c8
kgty0707 <kgty0707@gmail.com>
2024-04-16 12:14:49 +0900

    Bug: Sam Inference image data

--- AI_SERVER/AI/model/image_seg.py
+++ AI_SERVER/AI/model/image_seg.py
@@ -1,11 +1,17 @@
+from ultralytics import YOLO
+from PIL import Image
+import supervision
 
 # 모델 처리
 '''
 user_name, ori_image_path, mask_image_path, sclera_x, sclera_y, cornea_x, cornea_y, created_dt
 '''
 
-def get_segmentation_result():
+model = YOLO('./runs/detect/train93/weights/best.pt')
+
+def get_segmentation_result(file_fath):
     '''
     return ori_image_path, mask_image_path, sclera_x, sclera_y, cornea_x, cornea_y
     '''
+    result = model.predict(file_fath, save=True, imgsz=320, conf=0.5)
     ...
